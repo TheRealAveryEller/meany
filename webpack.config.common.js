4bcfad1b1a04d2174c11608902571f15c4b1c2d1
Avery Eller <j.eller1011@gmail.com>
2017-08-31 12:51:36 -0500

    Start Configuring Webpack

---
+++ webpack.config.common.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        'app': './assets/app/main.ts'
+    },
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ['awesome-typescript-loader', 'angular2-template-loader', 'angular2-router-loader']
+            },
+            {
+                test: /\html$/,
+                loader: html
+            },
+            {
+                test: /.css/,
+                loader: raw
+            }            
+        ]        
+    }
+};
